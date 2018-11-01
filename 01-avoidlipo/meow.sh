92977378720091040065ea0be53efe44a14f2cf7
Hugo <hugovk@users.noreply.github.com>
2018-11-01 12:46:15 +0200

    Update

--- 01-avoidlipo/meow.sh
+++ 01-avoidlipo/meow.sh
@@ -3,14 +3,15 @@
 echo $1 $2
 wc -w $1
 
-meow.py $1 > meow-$1
+python3 meow.py $1 > meow-$1
 wc -w meow-$1
 
-meow.py -t $1 > meow-x2-$1
+python3 meow.py -t $1 > meow-x2-$1
 wc -w meow-x2-$1
 
 echo "PDF:" meow-$1
 # https://openbookproject.googlecode.com/svn/tangle/html2pdf/reportlab_2_1/reportlab/tools/py2pdf/py2pdf.py
+# pip install reportlab
 py2pdf.py --fontSize 10 --mode mono --title "$2" meow-$1
 
 echo "PDF:" meow-x2-$1
